0f153d71883e6550375dbed50d6d53dbad5f5d02
Filipe Cajado <filipecajado@ucl.br>
2024-03-27 14:45:11 -0300

    added search

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,11 @@
 import { Aside } from '@/components/Aside';
 import { Prompt } from 'next/font/google';
 import './globals.css';
+import { SearchForm } from '@/components/SearchForm';
 
 const prompt = Prompt(
   {
-    weight: ['400', '600'],
+    weight: ['400', '500', '600'],
     subsets: ['latin'],
     display: 'swap',
   }
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
         <div className='app-container'>
           <Aside />
           <div className='main-content'>
+           <SearchForm />
              {children}
           </div>
         </div>
