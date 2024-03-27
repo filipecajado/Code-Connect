898b591f054a01099b01279efeb5f182e4d94461
Filipe Cajado <filipecajado@ucl.br>
2024-03-27 15:09:26 -0300

    added paginator with query params

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,24 @@ import styles from './page.module.css';
 import { logger } from "@/logger";
 import Link from 'next/link';
 import db from "../../prisma/db";
-async function getAllPost (page) {
+
+
+
+async function getAllPost (page, searchTerm) {
   try{
+
+    const where = {};
+
+    if (searchTerm){
+      where.title = {
+        contains: searchTerm,
+        mode: 'insensitive'
+      }
+    }
 
     const perPage = 6;
     const skip = (page -1) * perPage;
-    const totalItems = await db.post.count()
+    const totalItems = await db.post.count({ where })
     const totalPages = Math.ceil(totalItems / perPage);
     const prev = page > 1 ? page -1 : null;
     const next = page < totalPages ? page + 1 : null;
@@ -16,6 +28,7 @@ async function getAllPost (page) {
     const posts = await db.post.findMany({
       take: perPage,
       skip,
+      where,
       orderBy: { createdAt: 'desc'},
       include: {
         author:true
@@ -30,8 +43,9 @@ async function getAllPost (page) {
 }
 
 export default async function Home({searchParams}) {
-  const currentPage = parseInt(searchParams.page ?? 1);
-  const {data: posts, prev , next} = await getAllPost(currentPage);
+  const currentPage = parseInt(searchParams?.page ?? 1);
+  const searchTerm = searchParams?.q
+  const {data: posts, prev , next} = await getAllPost(currentPage, searchTerm);
 
   return (
     <main className={styles.grid}>
@@ -43,8 +57,8 @@ export default async function Home({searchParams}) {
       ))}
 
       <div className={styles.links}>
-       {prev && <Link href={`/?page=${prev}`}> Página anterior</Link>}
-        {next && <Link href={`/?page=${next}`}> Próxima página</Link>}
+       {prev && <Link href={{ pathname: '/', query: { page: prev, q: searchTerm}}}> Página anterior</Link>}
+        {next && <Link href={{ pathname: '/', query: { page: next, q: searchTerm}}}> Próxima página</Link>}
       </div>
     </main>
   );
